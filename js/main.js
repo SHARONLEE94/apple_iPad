d985c9f686932128328a4feb0245b7dbf56beb81
SHARONLEE94 <sharon940329@gmail.com>
2023-01-06 00:39:23 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+// 장바구니 dropDown
+const basketStarterEl = document.querySelector("header .basket-starter");
+const basketEl = basketStarterEl.querySelector(".basket");
+
+basketStarterEl.addEventListener("click", function(event) {
+  event.stopPropagation();
+  // 토글
+  if(basketEl.classList.contains("show")){
+    hideBasket();
+  } else{
+    showBasket();
+  }
+});
+
+basketEl.addEventListener("click", function(event) {
+  event.stopPropagation();
+});
+
+window.addEventListener("click", function() {
+  hideBasket();
+});
+
+function showBasket() {
+  basketEl.classList.add("show");
+}
+function hideBasket() {
+  basketEl.classList.remove("show");
+}
+
+
+// 검색
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
+
+
+
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
+
+
+function showSearch() {
+  headerEl.classList.add("searching");
+
+  document.documentElement.classList.add('fixed');
+
+  //순차적으로 나타남
+  headerMenuEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = (index *.4) / headerMenuEls.length + 's';
+  });
+
+  //순차적으로 나타남2
+  searchDelayEls.forEach(function(el, index) {
+    el.style.transitionDelay = (index * .4) / searchDelayEls.length + 's';
+  });
+
+  setTimeout(function() {
+    searchInputEl.focus();
+  },600);
+}
+
+function hideSearch() {
+  headerEl.classList.remove("searching");
+
+  document.documentElement.classList.remove('fixed');
+
+  // 순차적으로 사라짐
+  headerMenuEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = (index *.4) / headerMenuEls.length + 's';
+  });
+
+  //순차적으로 사라짐2
+  searchDelayEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = (index * .4) / searchDelayEls.length + 's';
+  });
+  searchDelayEls.reverse();
+  searchInputEl.value='';
+}
